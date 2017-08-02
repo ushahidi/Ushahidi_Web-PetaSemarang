8ed73fffbc5e750ca17e5878d2ff7dc98c9cf951
pekakota <pekakota@gmail.com>
2017-08-03 02:27:44 +0700

    Update page.php

--- themes/default/views/page.php
+++ themes/default/views/page.php
@@ -2,7 +2,7 @@
 	<div class="content-bg">
 		<div class="big-block">
 			<h1><?php echo html::escape($page_title) ?></h1>
-			<div class="page_text"><?php 
+			<div><?php 
 			echo $page_description;
 			Event::run('ushahidi_action.page_extra', $page_id);
 			?></div>
